7c8b5b3fea4cb332aa81e0fac254696cba03b476
in4nd0 <in4nd0@in4nd0-Nitro-AN515-58>
2023-12-04 16:15:36 -0500

    Correciones

--- SistemaBancario/src/test/java/com/empresa/SistemaBancario/SistemaBancarioApplicationTests.java
+++ SistemaBancario/src/test/java/com/empresa/SistemaBancario/SistemaBancarioApplicationTests.java
@@ -3,7 +3,7 @@ package com.empresa.SistemaBancario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SistemaBancarioApplicationTests.class)
 class SistemaBancarioApplicationTests {
 
 	@Test
